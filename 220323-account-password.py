590f252ea9a1546d2d0f1e3de09f24464b19fc3d
lucyl23 <qwqter4@gmail.com>
2025-04-04 17:11:49 +0800

    Create 220323-account-password.py

---
+++ 220323-account-password.py
@@ -0,0 +1,22 @@
+#帳號密碼與註解
+
+print('輸入帳號')  #print=cout
+acc=input()     #input=C++中的cin
+
+if acc == '21233':   #記得後面要加上冒號:
+  print('輸入密碼')
+  psw=input()
+  if psw=='10932159':
+    print('成功登入')
+  else :
+    print('登入失敗')
+
+else :
+  print('帳號不存在')
+
+
+'''
+太酷了吧
+三個點中間全部都是註解
+'''
+
